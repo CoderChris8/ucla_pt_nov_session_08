e9c329f6bda1258dcef3f79f6d643a262ab6f344
JD <jt7903@gmail.com>
2018-04-15 14:29:22 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: 'Our Awesome App',
+      another: 'Something Else',
+      count: 0
+    }
+  }
+
+  increaseCount() {
+    // console.log(this);
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {/* <h1>{this.state.title}</h1> */}
+        <Header 
+          title={this.state.title} 
+          another={this.state.another}
+          increase={this.increaseCount.bind(this)} />
+
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.increaseCount.bind(this)}>Increase Count</button>
+
+        <input type="text" 
+          name="title" 
+          value={this.state.title}
+          onChange={this.handleChange.bind(this)} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = (props) => (
+  <header>
+    <h3>{props.title}</h3>
+    <h4>{props.another}</h4>
+    <nav>
+      <a href="/">Home</a>
+      <a href="/contact">Contact</a>
+      <a href="/about">About</a>
+    </nav>
+    <button onClick={props.increase}>Increase</button>
+  </header>
+);
+
+export default Header;
